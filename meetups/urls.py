f64807f2ca82c738837b587fa850d2c6f0e8c8b3
mohsenBagherzadehAnsari <divinehemlock@gmail.com>
2022-07-17 03:59:09 +0430

    save at 2:50:36

--- meetups/urls.py
+++ meetups/urls.py
@@ -1,7 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from meetups import views
 
 urlpatterns = [
     path('meetups/', views.index, name='all-meetups'),  # our-domain.com/meetups
-    path('meetups/<slug:meetup_slug>', views.meetup_details , name='meetup-details')  # our-domain.com/meetups/<dynamic-path>
-]
+    path('meetups/<slug:meetup_slug>', views.meetup_detail, name='meetup-detail')  # our-domain.com/meetups/<dynamic-path>
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- meetups/views.py
+++ meetups/views.py
@@ -1,30 +1,21 @@
 from django.shortcuts import render
 
+from meetups.models import Meetups
+
 
 # Create your views here.
 
 def index(request):
-    meetups = [
-        {'title': 'a first meetup',
-         'location': 'paris',
-         'slug': 'a-first-meetup'},
-
-        {'title': 'a second meetup',
-         'location': 'new york',
-         'slug': 'a-second-meetup'}
-    ]
+    meetups = Meetups.objects.all()
     return render(request, 'meetups/index.html', {
         'show_meetup': True,
-        'meetups': meetups
+        'meetups': meetups,
     })
 
 
-def meetup_details(request , meetup_slug):
-    selected_meetup = {
-        'title': 'first meetup',
-        'description': 'details are displayed here for the meetup!'
-    }
-    return render(request, 'meetups/meetup-details.html', {
-        'meetup_title': selected_meetup['title'],
-        'meetup_description': selected_meetup['description']
+def meetup_detail(request, meetup_slug):
+    selected_meetup = Meetups.objects.get(slug=meetup_slug)
+    return render(request, 'meetups/meetup-detail.html', {
+        'meetup_title': selected_meetup.title,
+        'meetup_description': selected_meetup.description,
     })
