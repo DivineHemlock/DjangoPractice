2c86dd98c595f6aeaa194d19f89552afe1ac582c
mohsenBagherzadehAnsari <divinehemlock@gmail.com>
2022-07-18 00:38:39 +0430

    3:28:33

--- meetups/models.py
+++ meetups/models.py
@@ -12,12 +12,22 @@ class Location(models.Model):
         return f'{self.name} ({self.address})'
 
 
+class Participant(models.Model):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
+
+
 class Meetups(models.Model):
     title = models.CharField(max_length=200)
+    organiser_email = models.EmailField()
+    date = models.DateTimeField()
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to='images')
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    participants = models.ManyToManyField(Participant, blank=True, null=True)
 
     def __str__(self):
         return self.title
--- meetups/views.py
+++ meetups/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
 from meetups.models import Meetups
+from meetups.forms import RegistrationForm
 
 
 # Create your views here.
+
 
 def index(request):
     meetups = Meetups.objects.all()
@@ -15,7 +17,16 @@ def index(request):
 
 def meetup_detail(request, meetup_slug):
     selected_meetup = Meetups.objects.get(slug=meetup_slug)
-    return render(request, 'meetups/meetup-detail.html', {
-        'meetup_title': selected_meetup.title,
-        'meetup_description': selected_meetup.description,
-    })
+    if request.method == 'GET':
+        form = RegistrationForm()
+        return render(request, 'meetups/meetup-detail.html', {
+            'selected_meetup': selected_meetup,
+            'form': form
+        })
+    else:
+        form = RegistrationForm(request.POST)  #  takes the posted info from the form in meetup-detail.html and creates an object of
+                                               #  participant type (because in the RegistrationForm , the model is Participant)
+        if form.is_valid(): # checks to see if the provided data is valid
+            participant = form.save()
+            selected_meetup.participants.add(participant)
+
